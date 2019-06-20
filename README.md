62d0c2f19264ad759818eb240a4be5678ba941fc
Dave Alden <dpa99c@gmail.com>
2019-06-20 20:37:45 +0100

    Extend plugin to migrate references in AndroidManifest.xml and project.properties

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 cordova-plugin-androidx-adapter
 ===============================
 
-Cordova/Phonegap plugin to migrate any code which references the legacy [Android Support Library](https://developer.android.com/topic/libraries/support-library/index) to the new [AndroidX](https://developer.android.com/jetpack/androidx/migrate) mappings in a Cordova Android platform project.
+If your Cordova project contains plugins/libraries which reference both the Android Support Library and AndroidX, your Android build will fail because the two cannot live side-by-side in an Android build.
 
-This enables a Cordova project for which AndroidX has been enabled (e.g. using [cordova-plugin-androidx](https://github.com/dpa99c/cordova-plugin-androidx) to successfully build even if it contains plugins which reference the legacy Support Library.
+This plugin provides a shim to migrate references to the legacy [Android Support Library](https://developer.android.com/topic/libraries/support-library/index) to the new [AndroidX](https://developer.android.com/jetpack/androidx/migrate) mappings in a Cordova Android platform project.
 
-The plugin uses a hook script to replace any legacy Support Library Gradle artifact references in the `build.gradle` with the new [artifact mappings](https://developer.android.com/jetpack/androidx/migrate#artifact_mappings) and any legacy Support Library package name references in the Java source code with the new [class mappings](https://developer.android.com/jetpack/androidx/migrate#class_mappings).
+This enables a Cordova project for which AndroidX has been enabled (e.g. using [cordova-plugin-androidx](https://github.com/dpa99c/cordova-plugin-androidx)) to successfully build even if it contains plugins which reference the legacy Support Library.
+ 
+The plugin uses a hook script to replace any legacy Support Library references:
+- Gradle artifacts in `app/build.gradle` and `project.properties` with new [artifact mappings](https://developer.android.com/jetpack/androidx/migrate#artifact_mappings)
+- Class/package names in `AndroidManifest.xml` or the Java source code (of Cordova plugins) with new [class mappings](https://developer.android.com/jetpack/androidx/migrate#class_mappings).
 
 <!-- DONATE -->
 [![donate](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG_global.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZRD3W47HQ3EMJ)
--- apply.js
+++ apply.js
@@ -3,6 +3,8 @@ var ARTIFACT_MAPPINGS_FILE = "artifact-mappings.json";
 var CLASS_MAPPINGS_FILE = "class-mappings.json";
 var JAVA_SRC_PATH = "./platforms/android/app/src/main/java";
 var BUILD_GRADLE_PATH = "./platforms/android/app/build.gradle";
+var PROJECT_PROPERTIES_PATH = "./platforms/android/project.properties";
+var MANIFEST_PATH = "./platforms/android/app/src/main/AndroidManifest.xml";
 
 var deferral, fs, path, now, recursiveDir;
 
@@ -27,17 +29,30 @@ function run() {
 
     var startTime = now();
 
-    var buildGradle = fs.readFileSync(BUILD_GRADLE_PATH).toString(),
-        artifactMappings = JSON.parse(fs.readFileSync(path.join(__dirname, '.', ARTIFACT_MAPPINGS_FILE)).toString());
+    var artifactMappings = JSON.parse(fs.readFileSync(path.join(__dirname, '.', ARTIFACT_MAPPINGS_FILE)).toString()),
+        buildGradle = fs.readFileSync(BUILD_GRADLE_PATH).toString(),
+        projectProperties = fs.readFileSync(PROJECT_PROPERTIES_PATH).toString(),
+        androidManifest = fs.readFileSync(MANIFEST_PATH).toString();
 
-    for(var oldName in artifactMappings){
-        var oldRegExpStr = sanitiseForRegExp(oldName) + '[^"]+';
-        var newName = artifactMappings[oldName];
-        buildGradle = buildGradle.replace(new RegExp(oldRegExpStr, 'gm'), newName);
+    // Replace artifacts in build.gradle, project.properties & AndroidManifest.xml
+    for(var oldArtifactName in artifactMappings){
+        var newArtifactName = artifactMappings[oldArtifactName],
+            artifactRegExpStr = sanitiseForRegExp(oldArtifactName) + ':[0-9.+]+';
+        buildGradle = buildGradle.replace(new RegExp(artifactRegExpStr, 'gm'), newArtifactName);
+        projectProperties = projectProperties.replace(new RegExp(artifactRegExpStr, 'gm'), newArtifactName);
     }
     fs.writeFileSync(BUILD_GRADLE_PATH, buildGradle, 'utf8');
+    fs.writeFileSync(PROJECT_PROPERTIES_PATH, projectProperties, 'utf8');
 
     var classMappings = JSON.parse(fs.readFileSync(path.join(__dirname, '.', CLASS_MAPPINGS_FILE)).toString());
+
+    // Replace class/package names in AndroidManifest.xml
+    for (var oldClassName in classMappings){
+        androidManifest = androidManifest.replace(new RegExp(oldClassName, 'g'), classMappings[oldClassName]);
+    }
+    fs.writeFileSync(MANIFEST_PATH, androidManifest, 'utf8');
+
+    // Replace class/package names in source code
     recursiveDir(JAVA_SRC_PATH, [function(file, stats){
         return !file.match(".java");
     }], attempt(function(err, files){
@@ -45,8 +60,8 @@ function run() {
 
         for(var filePath of files){
             var fileContents = fs.readFileSync(filePath).toString();
-            for (var oldName in classMappings){
-                fileContents = fileContents.replace(new RegExp(oldName, 'g'), classMappings[oldName]);
+            for (var oldClassName in classMappings){
+                fileContents = fileContents.replace(new RegExp(oldClassName, 'g'), classMappings[oldClassName]);
             }
             fs.writeFileSync(filePath, fileContents, 'utf8');
         }
